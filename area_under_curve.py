4b4c156f0c0e2a12eadaaa9d40b55ff4c07567a8
ericmiller84143 <35577371+ericmiller84143@users.noreply.github.com>
2018-02-27 15:18:13 -0500

    Update area_under_curve.py
    worked on parabola stuff

--- area_under_curve.py
+++ area_under_curve.py
@@ -9,16 +9,35 @@ def line_area(intervals,x1,x2):
   area  = 0
   base = (x2-x1)/intervals
   for i in range(1,intervals+1):
-    height = base*(i-.5)*m + b #for y=x
+    x = base*(i-.5)
+    height = m*x+ b 
     area  = height*base + area
   integral = .5*m*(x2**2-x1**2)+b*(x2-x1)
   return area, integral
 
-
+def parabola_area(intervals, x1, x2):
+  a = 1
+  b=2
+  c=-2
+  area = 0
+  base = (x2-x1)/intervals
+  for i in range(1,intervals+1):
+    x = base*(i-.5)
+    height = a*(x**2) + b*x + c
+    area  = height*base + area
+  integral = 0 #need to calculte exact integral value
+  return area, integral 
 
 
 intervals = int(input("Enter the number of rectangles to use:"))
 
 x1=0
 x2=5
-rectangle_area, integral = line_area(intervals,x1,x2)
+rectangle_line_area, integral_line = line_area(intervals,x1,x2)
+rectangle_parabola_area, integral_parabola = parabola_area(intervals,x1,x2)
+
+
+
+#do some sort of loop to estimate area with a variety for the number of rectangles for each of the 3 functions
+#in (x,y) 2D array, keep appending the values of (n, E(n)) where E(n) is the error for a given number of rectangles
+#then plot each function
